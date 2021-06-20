7254057cf667f96115d1806bef65f5dc35324ab1
Shawiiz_z <shaweereso@gmail.com>
2021-06-21 00:49:47 +0200

    Make this tool better

--- index.js
+++ index.js
@@ -1,79 +1,29 @@
-const fs = require('fs')
-const execSync = require('child_process').execSync
-const path = require('path')
+const { AutoPull } = require('./util')
 
-const repoAccount = ""
-const repoName = ""
-const projectPath = ""
-const pullInterval = 30 //In seconds
-const githubUsername = ""
-const githubPassword = ""
-const githubOAuth = "" //Don't fill if you're using github credentials
+/*
+EXAMPLES
 
-let repoAuthUrl = "" //Don't fill
+########## FOR PRIVATE REPOSITORIES ##########
 
-async function main() {
-    log("Project Updater Started :D")
+new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds)).auth("username", "password").start()
+new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds)).oauth("oauthtoken").start()
 
-    if(githubUsername === "" && githubPassword === "" && githubOAuth == "") {
-        log("Please fill authentication informations !")
-        return
-    }
+new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds)).auth("username", "password").withBranch("branchName").start()
+new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds)).oauth("oauthtoken").withBranch("branchName").start()
 
-    let authUrlBase = githubOAuth
-    if(authUrlBase === "") authUrlBase = githubUsername + ':' + githubPassword
+########## FOR PUBLIC REPOSITORIES ##########
 
-    repoAuthUrl = '"https://' + authUrlBase + '@github.com/' + repoAccount + '/' + repoName + '.git"'
+new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds)).start()
+new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds)).withBranch("branchName").start()
 
-    if (!fs.existsSync(projectPath)) {
-        log("Folder doesn't exists, creating one...");
-        fs.mkdirSync(projectPath, { recursive: true })
-    }
+########## STOP ##########
 
-    if (!fs.existsSync(path.join(projectPath, '.git'))) {
-        if (!isDirEmpty(projectPath)) {
-            log("The directory at " + projectPath + " need to be empty to clone the project !")
-            return
-        }
+const pull = new AutoPull(...)
+pull.stop()
 
-        log("Project not cloned, cloning it. Please wait...")
+########## DISABLE GIT LOG ##########
 
-        execSync('git clone ' + repoAuthUrl + ' .', {
-            cwd: projectPath
-        })
-    }
+new AutoPull(...).disableGitLogs()
 
-    pull()
-    setInterval(function () {
-        pull()
-    }, (pullInterval*1000));
-}
-
-function pull() {
-    log('Pulling new data if needed...');
-
-    execSync('git reset --hard', {
-        cwd: projectPath  
-    })
-
-    const pullOut = execSync('git pull ' + repoAuthUrl, {
-        cwd: projectPath  
-    }).toString()
-
-    console.log("Out : "+pullOut);
-
-    if(!pullOut.includes('Already up to date.')) {
-        //Do an action here :)
-        //execSync('pm2 restart 0')
-    }
-}
-
-function log(msg) {
-    return console.log('[' + new Date().toLocaleString() + '] ' + msg)
-}
-
-function isDirEmpty(dirname) {
-    return fs.readdirSync(dirname).length === 0
-}
-
-main()
+You can also use global variables for authentication, but i let you do that yourself :)
+*/
---
+++ util.js
@@ -0,0 +1,115 @@
+const fs = require('fs')
+const execSync = require('child_process').execSync
+const path = require('path')
+
+class AutoPull {
+    constructor(repoAccount, repoName, projectPath, pullInterval) {
+        if (!repoAccount || !repoName || !projectPath || !pullInterval)
+            log("You have to specify repo account, repo name, project path and the pull interval.");
+        this.repoAccount = repoAccount
+        this.repoName = repoName
+        this.projectPath = projectPath
+        this.pullInterval = pullInterval
+        this.branch = ''
+        this.url = ''
+        this.gitlog = true
+        return this
+    }
+
+    oauth(token) {
+        if (!token) {
+            log("You have to specify username and password.", this);
+            return
+        }
+        this.url = '"https://' + token + '@github.com/' + this.repoAccount + '/' + this.epoName + '.git"'
+        return this
+    }
+
+    auth(username, password) {
+        if (!username || !password) {
+            log("You have to specify username and password.", this);
+            return
+        }
+        this.url = '"https://' + username + ':' + password + '@github.com/' + this.repoAccount + '/' + this.repoName + '.git"'
+        return this
+    }
+
+    withBranch(branch) {
+        this.branch = " -b " + branch
+        return this
+    }
+
+    disableGitLogs() {
+        this.gitlog = false
+        return this
+    }
+
+    start() {
+        if (!this.repoAccount || !this.repoName || !this.projectPath || !this.pullInterval) return
+        if (this.url === '') this.url = '"https://github.com/' + this.repoAccount + '/' + this.repoName + '.git"'
+
+        log('Started auto pull for repo https://github.com/' + this.repoAccount + '/' + this.repoName, this)
+
+        if (this.url)
+
+            if (!fs.existsSync(this.projectPath)) {
+                log("Folder doesn't exists, creating one...", this);
+                fs.mkdirSync(this.projectPath, { recursive: true })
+            }
+
+        if (!fs.existsSync(path.join(this.projectPath, '.git'))) {
+            if (!isDirEmpty(this.projectPath)) {
+                log("The directory at " + this.projectPath + " need to be empty to clone the project !", this)
+                return
+            }
+
+            log("Project not cloned, cloning it. Please wait...", this)
+
+            execSync('git clone ' + this.url + this.branch + ' .', {
+                cwd: this.projectPath
+            })
+        }
+
+        this.pull()
+        this.interval = setInterval(() => {
+            this.pull()
+        }, (this.pullInterval * 1000));
+        return this
+    }
+
+    stop() {
+        clearInterval(this.interval)
+        log('Stopped auto pull for repo https://github.com/' + this.repoAccount + '/' + this.repoName, this)
+        return this
+    }
+
+    pull() {
+        log('Pulling...', this);
+
+        execSync('git reset --hard', {
+            cwd: this.projectPath
+        })
+
+        const pullOut = execSync('git pull ' + this.url, {
+            cwd: this.projectPath,
+            stdio: 'pipe'
+        }).toString()
+
+        if (this.gitlog) log(pullOut, this);
+
+        if (!pullOut.includes('Already up to date.')) {
+            //Do an action here :)
+            //execSync('pm2 restart 0')
+        }
+    }
+}
+
+function log(msg, cl) {
+    return console.log('[' + new Date().toLocaleString() + (!cl ? '' : ' | ' + cl.repoAccount + '/' + cl.repoName) + ']', msg)
+}
+
+function isDirEmpty(dirname) {
+    return fs.readdirSync(dirname).length === 0
+}
+
+module.exports.AutoPull = AutoPull
